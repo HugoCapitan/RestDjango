83cbdb7bb775d9d0f67fb5d0bcb310eb776a0568
HugoCapitan <hmoravilez@gmail.com>
2016-06-02 17:38:28 -0500

    creating view for highlighted snippets

--- snippets/views.py
+++ snippets/views.py
@@ -1,9 +1,21 @@
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 from django.contrib.auth.models import User
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+	return Response({
+		'users': reverse('user-list', request=request, format=format),
+		'snippets': reverse('snippets-list', request=request, format=format)
+		})
 
 class UserList(generics.ListAPIView):
 	queryset = User.objects.all()
@@ -28,3 +40,11 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	serializer_class = SnippetSerializer
 	permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 	
+
+class SnippetHighlight(generics.GenericAPIView):
+	queryset = Snippet.objects.all()
+	renderer_classes = (renderers.StaticHTMLRenderer,)
+
+	def get(self, request, *args, **kwargs):
+		snippet = self.get_object()
+		return Response(snippet.highlighted)
